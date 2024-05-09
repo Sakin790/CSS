5f2b8f0a944e6c11f6b2d9312392dbc34221f253
Sakin790 <sakinislam79@gmail.com>
2024-05-09 19:49:30 +0600

    shadCn init

---
+++ 02_ShadCN/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <Button>Click </Button>
+  </div>
+</template>
