3e45bdf9ae22e514f2cafcc394190f2b1f9be1f0
Sakin790 <sakinislam79@gmail.com>
2024-05-10 11:22:46 +0600

    working

--- 03_Project/src/App.vue
+++ 03_Project/src/App.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { ref } from 'vue';
 
-function handleClick() {
-  alert('You clicked the button!');
-}
+let value = ref('');
+
+const handleClick = () => {
+  console.log(value.value);
+};
 </script>
 
 <template>
-  <div class="w-full h-screen bg-black ">
-    <Button variant="secondary" @click="handleClick">Click me</Button>
+  <div class="flex w-full h-screen justify-center max-w-sm items-center gap-1.5">
+    <Input id="email" type="email" placeholder="Email" v-model="value" />
+    <Button type="submit" @click="handleClick">
+      Print
+    </Button>
+    <h3>Message is: {{ value }}</h3>
   </div>
 </template>
