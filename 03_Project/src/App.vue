78e934b284012d499175e4bc02628de59c4de548
Sakin790 <sakinislam79@gmail.com>
2024-05-10 19:38:09 +0600

    working

--- 03_Project/src/App.vue
+++ 03_Project/src/App.vue
@@ -1,32 +1,22 @@
-<script setup lang="ts">
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { ref } from 'vue';
-import { reactive } from "vue"
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-let value = ref('');
+export default defineComponent({
+  name: "App",
+  component: {},
 
-const handleClick = () => {
-  console.log(value.value);
-};
-
-
-const user = reactive({
-  isLoggedIn: true,
-  isActive: true
-});
+  data() {
+    return {
+      score: 1
+    }
+  }
+})
 </script>
 
+
 <template>
-  <div class="flex w-full h-screen justify-center max-w-sm items-center gap-1.5">
-    <Input id="email" type="email" placeholder="Email" v-model="value" />
-    <Button type="submit" @click="handleClick">
-      Print
-    </Button>
-    <h3>Message is: {{ value }}</h3>
-    <div v-if="user.isLoggedIn">
-      <p>You r not loggedin</p>
-    </div>
+  <div>
+    <h1>Youre Score is : {{ score }}</h1>
 
   </div>
 </template>
