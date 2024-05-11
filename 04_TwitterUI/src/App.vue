ad16676bb7e85d768017bb33c487441c948276fa
Sakin790 <sakinislam79@gmail.com>
2024-05-11 19:58:33 +0600

    woking

--- 04_TwitterUI/src/App.vue
+++ 04_TwitterUI/src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <h1>Twitter UI Clone</h1>
+  <div class="grid  grid-cols-12 h-screen w-screen  bg-black ">
+    <div class="col-span-3 ">
+      <div
+        class="text-white  flex cursor-pointer font-bold justify-center mt-2 hover:bg-slate-600  transition-all rounded-full  h-fit p-2">
+        <h4>Twitter</h4>
+      </div>
+    </div>
+    <div class="col-span-6 border-r-[1px] border-l-[1px] border-gray-500 ">Sakin</div>
+    <div class="col-span-3">
+      <div>
+        <h2>Facebook</h2>
+      </div>
+    </div>
   </div>
 </template>
