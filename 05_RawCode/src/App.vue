29b2f23a1e67869ebfef8b38b44c6f1a54d4faed
Sakin790 <sakinislam79@gmail.com>
2024-05-11 23:27:17 +0600

    learning

--- 05_RawCode/src/App.vue
+++ 05_RawCode/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <h1>Audio Template</h1>
-    <audio controls>
-      <source src="./sample.mp3" type="audio/mpeg">
-      <source src="./sample.ogg" type="audio/ogg">
-      Your browser does not support the audio element.
-    </audio>
-  </div>
+    <div>
+        <h1>Audio Template</h1>
+        <audio controls>
+            <source src="./sample.mp3" type="audio/mpeg">
+            <source src="./sample.ogg" type="audio/ogg">
+            Your browser does not support the audio element.
+        </audio>
+    </div>
 </template>
