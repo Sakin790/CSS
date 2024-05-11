5eec0b5ca2636fb8c5d7c6ee3e6d59aca8029010
Sakin790 <sakinislam79@gmail.com>
2024-05-11 21:17:00 +0600

    audio working

--- 04_TwitterUI/src/App.vue
+++ 04_TwitterUI/src/App.vue
@@ -5,6 +5,7 @@
         class="text-white  flex cursor-pointer font-bold justify-center mt-2 hover:bg-slate-600  transition-all rounded-full  h-fit p-2">
         <h4>Twitter</h4>
       </div>
+      <h2 class="text-white font-bold ml-20 ">Home</h2>
     </div>
     <div class="col-span-6 border-r-[1px] border-l-[1px] border-gray-500 ">Sakin</div>
     <div class="col-span-3">
